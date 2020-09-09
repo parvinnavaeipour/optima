44c513a0cae7a5716dd6088ed22290b75f31fe5b
parvinnavaeipour <14pn4@queensu.ca>
2020-09-09 10:57:51 -0400

    Updates

---
+++ precache-manifest.fdb8e6aff50fd658524b6641e0781fb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a4e0669071a11841e9a28dbf8eb3c8",
+    "url": "/optima/index.html"
+  },
+  {
+    "revision": "9d8caa30e0c3242f073d",
+    "url": "/optima/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c4619cb8bc29867ba4e",
+    "url": "/optima/static/js/2.8d19b154.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/optima/static/js/2.8d19b154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d8caa30e0c3242f073d",
+    "url": "/optima/static/js/main.db085ffa.chunk.js"
+  },
+  {
+    "revision": "0e2af1dbe688ea1454c1",
+    "url": "/optima/static/js/runtime-main.d705e63e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/optima/static/media/logo.5d5d9eef.svg"
+  }
+]);
